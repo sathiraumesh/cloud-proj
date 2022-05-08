6b840979b6c94db0ed9f466c1492788f73237e50
sathiraumesh <sathira@sathiras-MacBook-Pro.local>
2022-05-08 17:27:42 +0530

    addedchanges

--- mymap-client/src/environments/environment.prod.ts
+++ mymap-client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api:'http://35.184.129.252:8080/api/',
+  api:'http://localhost:8080/api/',
   redrict_url: 'https://jolly-hill-04111cd10.1.azurestaticapps.net',
   production: true
 };
